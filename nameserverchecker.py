36b67e9d8874e2d663c5da3d1b740d6109c00317
Alex Lunnon <alex@nder.com.au>
2015-12-09 11:03:18 +1100

    hello world

---
+++ nameserverchecker.py
@@ -0,0 +1,37 @@
+from urlparse import urlparse
+from threading import Thread
+import httplib, sys, os, re, commands
+
+from Queue import Queue
+
+concurrent = 25
+success = 0
+errors = 0
+
+def doWork():
+    while True:
+        url = q.get()
+        whoisresult = getStatus(url)
+        doSomethingWithResult(whoisresult, url)
+        q.task_done()
+
+def getStatus(ourl):
+    try:
+        whoisresult = commands.getoutput("dig ns " + ourl + " +short") #get whois information
+        return whoisresult
+    except:
+        error = 1
+def doSomethingWithResult(whoisresult, domain):
+    print domain + ",\"" + whoisresult.replace('\n', ' ') + "\""
+q = Queue(concurrent * 2)
+
+for i in range(concurrent):
+    t = Thread(target=doWork)
+    t.daemon = True
+    t.start()
+try:
+    for url in open('alldomains.csv'):
+        q.put(url.strip())
+    q.join()
+except KeyboardInterrupt:
+    sys.exit(1)
